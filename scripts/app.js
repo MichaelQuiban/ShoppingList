b766df5fceccb50d28f50f062f857c662dd2b1fc
Michael Q <MichaelQuiban@outlook.com>
2016-06-30 23:54:35 -0700

    Added more area for the results, a little changes on sizing, and a results header for the user input

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,9 @@
 function addShoppingItem() {
-	var text = $('input[name=option-text').val();
-	$('#todolist').append('<li>'+text+' <button>X</button></li>');
-
+	var item = $("#option-text1").val();
+	var price = $("#option-text2").val();
+	var quantity = $("#option-text3").val();
+	var location = $("#option-text4").val();
+	$("#todolist").append('<li>'+item+','+price+','+quantity+','+location+' <button>X</button></li>');
 }
 
 $(function() {
