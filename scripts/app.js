9eb8db27c0c097a472bf9136e419a2d47307b8fe
Michael Q <MichaelQuiban@outlook.com>
2016-07-02 04:03:00 -0700

    Added Delete functionality on user input, removed default styling on delete buttons

--- scripts/app.js
+++ scripts/app.js
@@ -4,10 +4,15 @@ function addShoppingItem() {
     var quantity = $("#quantity-text").val();
     var location = $("#location-text").val();
     $("#todolist").append('<li>' + item + ',' + price + ',' + quantity +
-        ',' + location + ' <button>X</button></li>');
+        ',' + location + ' <button class="delete">X</button></li>');
     $("#item-text").val("");
     $("#price-text").val("");
     $("#quantity-text").val("");
     $("#location-text").val("");
 }
+function deleteItem(){
+    $(this).parent().remove();
+}
+
 $("#submit-btn").on('click', addShoppingItem)
+$(".delete").on('click', deleteItem);
